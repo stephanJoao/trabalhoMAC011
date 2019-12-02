05a6155f0cd9f9e8399d13eb7d211469da217453
ICE <ice@lab03m18.nrc.ice.ufjf.br>
2019-12-02 16:43:05 -0300

    Trabalho pronto

--- Trab3IntroducaoEngComp.py
+++ Trab3IntroducaoEngComp.py
@@ -12,21 +12,22 @@ Integrantes: Alexandre Vitor Silva Braga - 201965501B
 
              Marcelo Ian  
 
-
-DESCRIÇÃO DO PROBLEMA: 
-
-Deseja-se encontrar, dada determinada locadora de carros com sede em Juiz de Fora e em Belo Horizonte, 
-
-a distribuição que melhor atenda à demanda de veículos em cada cidade, com base em registros históricos 
-
-de aluguel e devolução de carros da empresa. Para tal, será utilizado o modelo da Cadeia de Markov 
-
-para a resolução do problema proposto, a qual define que, dado qualquer evento passado e o estado presente, 
-
-a probabilidade desse evento depende somente do estado presente, ou seja, independe de eventos passados. 
-
-
 '''
+import matplotlib.pyplot as plt
+
+x = [0]
+
+yA1 = [7000]
+yA2 = [0]
+
+yB1 = [5000]
+yB2 = [2000]
+
+yC1 = [2000]
+yC2 = [5000]
+
+yD1 = [0]
+yD2 = [7000]
 
 P = [0.6, 0.4, 0.3, 0.7] #P representa uma matriz 2x2 
 
@@ -36,44 +37,19 @@ print("P = [(", P[0], ",", P[1], "), ")
 
 print("     (", P[2], ",", P[3], ")]")
 
-''' 
+print("\nEventos iniciais: ")
 
-O objetivo é encontrar um equilíbrio entre as probabilidades. 
+print("A = [7000, 0]\nB = [5000, 2000]\nC = [2000, 5000]\nD = [0, 7000]")
 
-Para isso, multiplica-se a matriz por ela mesma até que a matriz resultado não mude mais. 
-
-P[índice] = P[B|A]: probabilidade do carro ser devolvido na cidade B dado que foi alugado na cidade A 
-
-P[0] = P[JF|JF] 
-
-P[1] = P[BH|JF] 
-
-P[2] = P[JF|BH] 
-
-P[3] = P[BH|BH] 
-
-'''
-
- 
 dias = 14 
-
-
-'''
-
-Como o tempo é discreto, após uma certa quantidade de dias,
-
-a saída será sempre a mesma para todo e qualquer caso na matriz de probabilidades,
-
-pois essas aproximar-se-ão de um valor probabilístico fixo. 
-
-'''
-
 
 U = [0.6, 0.4, 0.3, 0.7] 
 V = [0.0, 0.0, 0.0, 0.0]
 
 for i  in range(dias): 
-
+    
+    x.append(i + 1)
+        
     V[0] = P[0] 
 
     V[1] = P[1] 
@@ -90,94 +66,65 @@ for i  in range(dias):
 
     P[3] = V[2]*U[1] + V[3]*U[3] 
 
-print("Probabilidade final:")
+    print("\n\nProbabilidade no dia",i + 1,":")
+    
+    print("P = [(", P[0], ",", P[1], "), ")
+    
+    print("     (", P[2], ",", P[3], ")]\n")
 
-print("P = [(", P[0], ",", P[1], "), ")
-
-print("     (", P[2], ",", P[3], ")]")
- 
-
-'''
-
-Atualização da Matriz para a 14ª potência,
-
-os valores matriciais probabilísticos aproximam-se de  [0.43, 0.57, 0.43, 0.57] 
-
-'''
-
-print("Eventos (estágio inicial): ")
-
-print("A = [7000, 0]\nB = [5000, 2000]\nC = [2000, 5000]\nD = [0, 7000]")
-
-print("Eventos (estágio final): ")
-
-
-#Situação 1 - 7000 em JF e 0 em BH 
-
-A = list() 
-
-S = [7000, 0] 
-
-A.append(S[0]*P[0]+S[1]*P[2]) 
-
-A.append(S[0]*P[1]+S[1]*P[3]) 
-
-print("A =", A) 
- 
-
-#Situação 2 - 5000 em JF e 2000 em BH 
-
-B = list() 
-
-S = [5000, 2000] 
-
-B.append(S[0]*P[0]+S[1]*P[2]) 
-
-B.append(S[0]*P[1]+S[1]*P[3]) 
-
-print("B =", B) 
- 
-
-#Situação 3 - 2000 em JF e 5000 em BH 
-
-C = list() 
-
-S = [2000, 5000] 
-
-C.append(S[0]*P[0]+S[1]*P[2]) 
-
-C.append(S[0]*P[1]+S[1]*P[3]) 
-
-print("C =", C) 
- 
-
-#Situação 4 - 0 em JF e 7000 em BH 
-
-D = list() 
-
-S = [0, 7000] 
-
-D.append(S[0]*P[0]+S[1]*P[2]) 
-
-D.append(S[0]*P[1]+S[1]*P[3]) 
-
-print("D =", D) 
- 
-
-''' 
-
-Como observado na execução do código, o ponto de equilíbrio encontrado é,
-
-respectivamente, a presença de 3000 carros em JF e 4000 carros em BH, 
-
-números nos quais as probabilidades atreladas à matriz tenderão a resultar.
-
-Por conseguinte, essa se mostra a melhor distribuição de carros entre as cidades. 
-
-'''
+    #Situação 1 - 7000 em JF e 0 em BH 
+    
+    S = [7000, 0] 
+    
+    A = [S[0]*P[0]+S[1]*P[2], S[0]*P[1]+S[1]*P[3]] 
+    
+    yA1.append(A[0])
+    
+    yA2.append(A[1])
+    
+    print("Carros no dia", i + 1,": ")
+    
+    print("A =", A) 
+     
+    #Situação 2 - 5000 em JF e 2000 em BH 
+    
+    S = [5000, 2000] 
+    
+    B = [S[0]*P[0]+S[1]*P[2], S[0]*P[1]+S[1]*P[3]] 
+    
+    yB1.append(B[0])
+    
+    yB2.append(B[1])
+    
+    print("B =", B) 
+    
+    #Situação 3 - 2000 em JF e 5000 em BH 
+    
+    S = [2000, 5000] 
+    
+    C = [S[0]*P[0]+S[1]*P[2], S[0]*P[1]+S[1]*P[3]] 
+    
+    yC1.append(C[0])
+    
+    yC2.append(C[1])
+    
+    print("C =", C) 
+    
+    #Situação 4 - 0 em JF e 7000 em BH 
+    
+    S = [0, 7000] 
+    
+    D = [S[0]*P[0]+S[1]*P[2], S[0]*P[1]+S[1]*P[3]] 
+    
+    yD1.append(D[0])
+    
+    yD2.append(D[1])
+    
+    print("D =", D) 
 
 
-print("Verifica-se que a soma das probabilidades em cada caso é constante, como o esperado:") 
+
+print("\n\n\nVerifica-se que a soma das probabilidades em cada caso é constante, como o esperado:") 
 
 print("Soma(A) =", A[0]+A[1]) 
 
@@ -187,4 +134,19 @@ print("Soma(C) =", C[0]+C[1])
 
 print("Soma(D) =", D[0]+D[1])
 
- 
+
+plt.plot(x, yA1, '-', color = 'red', label = 'JF situação 1')
+plt.plot(x, yA2, '--',  color = 'darkred', label = 'BH situação 1')
+plt.plot(x, yB1, '-', color = 'limegreen', label = 'JF situação 2')
+plt.plot(x, yB2, '--', color = 'darkgreen', label = 'BH situação 2')
+plt.plot(x, yC1, '-', color = 'royalblue', label = 'JF situação 3')
+plt.plot(x, yC2, '--', color = 'darkblue', label = 'BH situação 3')
+plt.plot(x, yD1, '-', color = 'gold', label = 'JF situação 4')
+plt.plot(x, yD2, '--', color = 'darkgoldenrod', label = 'BH situação 4')
+
+plt.legend()
+plt.xlabel('Número de dias')
+plt.ylabel('Número de carros')
+plt.grid()
+plt.savefig('grafico.pdf')
+plt.show()
